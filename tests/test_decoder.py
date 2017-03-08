60c7d335e6fba28de953f6b7b3fd7f06e48bc4ab
unknown <unknown321@users.noreply.github.com>
2017-03-08 06:33:16 +0300

    more decoder tests

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -51,5 +51,52 @@ c4M2QoF5juLcqskL1iLDYQlLVsTH5VCgC7mK204ygBrK6BopI6RZN6pX+6R\
 		self.assertTrue(result==r)
 
 	def test_decode_with_compress_with_encrypt(self):
-		pass
+		encoded_input = 'YnHdLj/1b4SBvSa1/0bYhcd4UAB70VUxyHv6whR4ARq0VmzU+tVGsFoR9Wd4Gi+WxsgVOVVnSXxScgtvvGefUbX7G3Ak0d01UYtsI2BBRDIHJULc8i/SM4/pMDTr3TQjtVwdHMlJi+z5PQEduWGyiSEMDW7JT8Q/6Tha3CBhup4kxzlfvtn+geZRWZHIiUxLNBmk7ozQeMm2qp+pmARHCFcdit7ETqtxREhrJSw2VymRFZFu6RuEuB/I9LK1ZHtUyDxKW0KHwLOyE35tXGFlzJrSB7Ptvc0nIg5S+ye/6oH9dH6akUdStMpxQ9nxYSmBPMUG3hbf3ocRZv14UO4wVfhT9jvWz+UPI+GRWSFrsJ7XPoBpTietoOGi7Tf32SsmwvCD7O9lSq6nm+ScqNdUorCc6RzotY2t1RDb/ekMbJDrhqBV++bczGf6G/YUvLmdnsAlzQt7YDRpzTvu0nOD4Ht3Towsb+7bBVwkqMUCxn9Tt5T1ic2LNZsO1NTpiJGNQo5uZTc/LEshTA4dD84oFh8aPF1O4SFZ4ZPs9ogZcDqGxs/vrdndfRXmV9zzPggW5J5kmMhKqKc='
+		result = {
+			'compress': True,
+			'original_size': 300,
+			'session_key': '',
+			'data': {
+				'flowid': None,
+				'crypto_type': 'COMPOUND',
+				'msgid': 'CMD_GET_FOB_STATUS',
+				'xuid': None,
+				'rqid': 0,
+				'fob_index': -1,
+				'record': {
+					'shield_date': 0,
+					'sneak': {
+						'lose': 0,
+						'win': 0
+					},
+					'defense': {
+						'lose': 0,
+						'win': 0
+					},
+					'score': 0,
+					'insurance': 0
+				},
+				'sneak_mode': -1,
+				'is_rescue': 0,
+				'is_reward': 0,
+				'result': 'NOERR',
+				'kill_count': 0
+			},
+			'session_crypto': True
+			}
+
+		d = decoder.Decoder(crypto_key=bytearray(base64.decodestring(settings.TESTS_MAGIC_HASH.encode())))
+		r = d.decode(encoded_input)
+		r['session_key'] = ''
+		r['data']['record']['sneak']['lose'] = 0
+		r['data']['record']['sneak']['win'] = 0
+		r['data']['record']['defense']['lose'] = 0
+		r['data']['record']['defense']['win'] = 0
+		r['data']['record']['score'] = 0
+		r['data']['kill_count'] = 0
+		self.assertTrue(result==r)
+
+
+
+
 
