aceab63ab5b452e27d28cbe38fbe592784dda873
unknown321 <you@example.com>
2017-12-17 13:28:37 +0300

    added function checks for proxy handler

--- src/mgsv_emulator/proxy_handler/ProxyHandler.py
+++ src/mgsv_emulator/proxy_handler/ProxyHandler.py
@@ -6,22 +6,36 @@ logger = logging.getLogger(settings.LOGGER_NAME)
 
 
 class ProxyHandler(object):
+    """
+    A class which executes functions before and after proxying the main function
+    """
     def __init__(self, command_name, command_data, player=None):
         self._command_name = command_name
         self._command_data = command_data
         self._player = player
 
+    def _get_function(self, ftype):
+        result = None
+        logger.debug('Looking for command {}'.format(self._command_name))
+        try:
+            mod = __import__('mgsv_emulator.proxy_handler.command.{}'.format(self._command_name),
+                    fromlist=["PROXY_PRE_{}".format(self._command_name)])
+        except Exception as e:
+            logger.debug('Cannot import module {}: {}'.format(self._command_name, str(e)))
+        else:
+            try:
+                command = getattr(mod, "{}{}".format(ftype, self._command_name))
+            except Exception as e:
+                logger.debug('Cannot import function {}{}'.format(ftype,self._command_name))
+            else:
+                if callable(command):
+                    result = command
+        return result
+
     def preprocess(self):
-        logger.info('I am preprocess')
-        logger.info('Got a proxyfied preprocess command to handle: {}'.format(self._command_name))
-        mod = __import__('mgsv_emulator.proxy_handler.command.{}'.format(self._command_name), fromlist=["PROXY_PRE_{}".format(self._command_name)])
-        command = getattr(mod, "PROXY_PRE_{}".format(self._command_name))
+        command = self._get_function("PROXY_PRE_")
         command.execute(data=self._command_data)
 
     def postprocess(self, _data=None):
-        logger.info('I am postprocess')
-
-        logger.info('Got a proxyfied preprocess command to handle: {}'.format(self._command_name))
-        mod = __import__('mgsv_emulator.proxy_handler.command.{}'.format(self._command_name), fromlist=["PROXY_POST_{}".format(self._command_name)])
-        command = getattr(mod, "PROXY_POST_{}".format(self._command_name))
+        command = self._get_function("PROXY_POST_")
         command.execute(data=_data)
